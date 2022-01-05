c74fdba7474e25e9c3385ed0e2c158d8c8a15859
Dominik Roth <dominik@roth.systems>
2022-01-05 15:24:12 +0100

    feat(Tests, Documentation, intellij configuration, package.json): Tests updated, Added documentation, Added IntelliJ configuration, Updated package.json
    Tests:
    Updated tests with better checks.
    Added getSingleAssetAmount check.
    Added comments as a placeholder for future checks.
    
    Doc:
    I've added the recommended docstrings for classes, files, etc.
    Added ToDo comments.
    
    IntelliJ Configuration:
    Added recommended files from the IntelliJ configuration folder.
    
    package.json:
    Updated package.json to feature "@types/chai" as a dev dependency.
    
    BREAKING CHANGE: Updated expects and checks may have unforeseen problems and may be breaking.

--- src/test.ts
+++ src/test.ts
@@ -1,28 +1,29 @@
+/* eslint-disable max-len */
+/**
+ * Defines the tests to be run on the picasso parachain node.
+ *
+ * Index:
+ *    endpoint:string
+ *    provider:WsProvider
+ *    api:ApiPromise
+ *
+ *    before()              Gets called before the tests and connects the API object.
+ *    after()               Gets called after all tests are finished and disconnects from the API.
+ *
+ *    describe()            Runs the tests using Mocha
+ *        1. it()           checkBalance() => Get balance and checks if > 0.
+ *        2. it()           listAssetAmounts() => Gets all assets on account and lists them.
+ *        3. it()
+ *        4. it()
+ **/
+
 import {ApiPromise, WsProvider} from '@polkadot/api';
 import {AccountTests} from './accountTests';
 
-
-/*
-Index:
-    endpoint:string
-    provider:WsProvider
-    api:ApiPromise
-
-    before()        Gets called before the tests and connects the API object.
-    after()         Gets called after all tests are finished and disconnects from the API.
-
-    describe()      Runs the tests using Mocha
-        1. it()     checkBalance() => Get balance and checks if > 0.
-        2. it()     listAssetAmounts() => Gets all assets on account and lists them.
-        3. it()
-        4. it()
- */
-
-// Initialise the provider to connect to the defined node
+// ToDo: Change endpoint to be read from env variables or run parameters.
 const endpoint = 'ws://127.0.0.1:9988';
 const provider = new WsProvider(endpoint);
-let api;
-
+let api:ApiPromise;
 
 before(async () => {
   api = await ApiPromise.create({provider: provider});
@@ -33,15 +34,34 @@ after(async () => {
 });
 
 describe('Account Tests', () => {
-  // Account balance check.
-  it('Balance check should result >0', async () => {
+  // Standard wallet tests
+  it('Wallet balance check should result >0', async () => {
     await AccountTests.checkBalance(api);
   });
 
-  it('List asset amounts', async () => {
-    await AccountTests.listAssetAmounts(api);
+  // Asset tests
+  it('Get single asset amount', async () => {
+    await AccountTests.getSingleAssetAmount(api);
   });
 
+  it('Get list of asset amounts', async () => {
+    await AccountTests.getListAssetAmounts(api);
+  });
+
+  // ToDo
+  // Governance Tests
+
+  // ToDo
+  // Vault 101 Tests
+
+  // ToDo
+  // Swap Tests
+
+  // ToDo
+  // Liquidity Tests
+
+  // ToDo
+  // Vesting tests
   it('Vesting creation test', async () => {
     // ToDo: Stub
   });
