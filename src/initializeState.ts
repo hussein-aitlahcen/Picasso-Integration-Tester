2e503302d680b2038b370417580c6e362cd560ec
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-01-05 20:14:21 +0100

    feat(crowdloan): Add simple crowdloan generation

--- src/initializeState.ts
+++ src/initializeState.ts
@@ -5,6 +5,7 @@
  **/
 import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
 import { KeyringPair } from '@polkadot/keyring/types';
+import * as R from 'ramda';
 
 // ToDo: Change endpoint to be read from env variables or run parameters.
 const endpoint = 'ws://127.0.0.1:9988';
@@ -31,6 +32,68 @@ class Generator {
     console.debug('Transfer sent with hash', hash.toHex());
   }
 
+  public static async testCrowdloanRewards(api: ApiPromise, sudoKey: KeyringPair) {
+    /*
+      Populate
+      Initialize
+      Associate (automatically execute first claim)
+     */
+
+    const toHexString = bytes =>
+      Array.prototype.map.call(bytes, x => ('0' + (x & 0xFF).toString(16)).slice(-2)).join('');
+
+    const delay = () => new Promise(res => setTimeout(res, 24_000));
+
+    const vesting48weeks = 100800;
+    const accounts =
+      R.unfold(n => n > 100 ? false : [[
+        { RelayChain: walletAlice.derive("/contributor-" + n.toString()).publicKey },
+        n * 1_000_000_000_000,
+        vesting48weeks
+      ], n + 1], 1);
+
+    const populateHash =
+      await api.tx.sudo.sudo(
+        api.tx.crowdloanRewards.populate(accounts)
+      ).signAndSend(sudoKey);
+    console.debug('Populated crowdloan with hash: ', populateHash.toHex());
+
+    await delay();
+
+    const initializeHash =
+      await api.tx.sudo.sudo(
+        api.tx.crowdloanRewards.initialize()
+      ).signAndSend(sudoKey);
+
+    console.debug('Initialized crowdloan with hash: ', initializeHash.toHex());
+
+    await delay();
+
+    const contributor: KeyringPair = walletAlice.derive("/contributor-1");
+    console.log('Contributor public key: ' + toHexString(contributor.publicKey));
+
+    const contributorRewardAccount: KeyringPair = contributor.derive("/reward");
+    console.log('Contributor reward account public key: ' + toHexString(contributorRewardAccount.publicKey));
+
+    const message = "<Bytes>picasso-" + toHexString(contributorRewardAccount.publicKey) + "</Bytes>";
+    console.log('Message to sign: ' + message);
+
+    const proof = contributor.sign(message);
+    console.log('Proof: ' + proof);
+
+    const associateHash =
+      await api.tx.sudo.sudo(
+        api.tx.crowdloanRewards.associate(
+          contributorRewardAccount.publicKey,
+          { RelayChain: [contributor.publicKey, { Sr25519: proof }]}
+        )
+      ).signAndSend(sudoKey);
+
+    console.debug('Associated with hash: ', associateHash.toHex());
+
+    //TODO(hussein-aitlahcen): check that we got the upfront liquidity in the reward account.
+  }
+
   // ToDo: Add more generation functions here.
 }
 
@@ -40,7 +103,7 @@ class Generator {
 **/
 async function createDefaultData(api: ApiPromise, sudoKey: KeyringPair) {
   await Generator.testTransaction(api);
-  
+  await Generator.testCrowdloanRewards(api, sudoKey);
   // ToDo: Add additional data generator calls here.
 }
 
