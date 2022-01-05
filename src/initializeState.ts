aa539366b9920ef70d41a7f5f0ef2f0c528f7693
Dominik Roth <dominik@roth.systems>
2022-01-05 16:23:39 +0100

    feat(initializeState.ts): Created base for initializeState.ts
    Created a base structure for initializeState.ts.
    Add additional generation functions to the Generator class, and call them from createDefaultData().

--- src/initializeState.ts
+++ src/initializeState.ts
@@ -1,2 +1,72 @@
-// ToDo: Stub
-console.warn('initializeState: STUB');
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable max-len */
+/**
+ * Inserts default values into the devnet for development.
+ **/
+import {ApiPromise, Keyring, WsProvider} from '@polkadot/api';
+
+// ToDo: Change endpoint to be read from env variables or run parameters.
+const endpoint = 'ws://127.0.0.1:9988';
+const provider = new WsProvider(endpoint);
+
+let walletAlice;
+let walletBob;
+let walletCharlie;
+let walletDave;
+let walletEve;
+let walletFerdie;
+
+/**
+ * Contains all generation methods as static async functions
+**/
+class Generator {
+  /**
+   * Sends test transaction from Alice to Bob.
+   * @param {ApiPromise} api Connected API Promise.
+  **/
+  public static async testTransaction(api:ApiPromise) {
+    const transfer = api.tx.assets.transferNative(walletBob.address, 12345678910, true);
+    const hash = await transfer.signAndSend(walletAlice, {nonce: -1});
+    console.debug('Transfer sent with hash', hash.toHex());
+  }
+
+  // ToDo: Add more generation functions here.
+}
+
+/**
+ * Call default generation here.
+ * @param {ApiPromise} api Connected API Promise.
+**/
+async function createDefaultData(api:ApiPromise) {
+  await Generator.testTransaction(api);
+  
+  // ToDo: Add additional data generator calls here.
+}
+
+/**
+ * Application entry point
+**/
+async function main() {
+  // Instantiate the API
+  const api = await ApiPromise.create({provider: provider});
+  // Constuct the keyring after the API (crypto has an async init)
+  const keyring = new Keyring({type: 'sr25519'});
+
+  /*  Get keys for dev accounts.
+      ToDo: Read public/private keys from external file to be usable in live environment.
+            and ability to specify keys using env variables or using run parameters.
+  */
+  walletAlice = keyring.addFromUri('//Alice');
+  walletBob = keyring.addFromUri('//Bob');
+  walletCharlie = keyring.addFromUri('//Charlie');
+  walletDave = keyring.addFromUri('//Dave');
+  walletEve = keyring.addFromUri('//Eve');
+  walletFerdie = keyring.addFromUri('//Ferdie');
+
+  console.info('Creating dummy data...');
+
+  await createDefaultData(api);
+
+  console.info('Finished creating dummy data.');
+}
+main().catch(console.error).finally(() => process.exit());
